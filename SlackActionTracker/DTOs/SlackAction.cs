91cb69f1b4ff05ba33b2ca602249861efc4f4972
Ozzy899 <ozren.petrovic.89@gmail.com>
2026-03-15 21:21:14 +0100

    Delete SlackActionTracker/DTOs/SlackAction.cs

--- SlackActionTracker/DTOs/SlackAction.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace SlackActionTracker.DTOs
-{
-    public class SlackAction
-    {
-        public string action_id { get; set; } = string.Empty;
-        public string value { get; set; } = string.Empty;
-        public string action_ts { get; set; } = string.Empty;
-    }
-}
